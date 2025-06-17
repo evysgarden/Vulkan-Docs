f5f1a478e9f97e5f9e9a7421656c2d5b4d32a7d8
Evy Garden <evysgarden@protonmail.com>
2025-06-18 01:41:59 +0200

    replace code and strong

--- flake.nix
+++ flake.nix
@@ -15,13 +15,43 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+
       in
       {
-        devShell = pkgs.mkShellNoCC {
-          packages = with pkgs; [
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "vulkan-man-pages";
+          version = "1.4.309";
+          src = ./.;
+
+          enableParallelBuilding = true;
+
+          nativeBuildInputs = with pkgs; [
+            asciidoctor-with-extensions
+            nodejs_24
             python3
             python3Packages.pyparsing
+            python3Packages.networkx
+            nodePackages.he
+            hexapdf
           ];
+
+          makeFlags = "man3pages";
+
+          postBuild = ''
+            substituteInPlace gen/out/man/man3/* \
+              --replace-quiet "<code>" "\fB" \
+              --replace-quiet "</code>" "\fP" \
+              --replace-quiet "<strong>" "\fI" \
+              --replace-quiet "<strong class=\"purple\">" "\fI" \
+              --replace-quiet "</strong>" "\fP" \
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/share/
+            cp -rv gen/out/man $out/share/man
+            runHook postInstall
+          '';
         };
       }
     );
